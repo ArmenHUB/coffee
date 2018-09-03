cd5ebae4a2d49e78c789b037b173e104640da202
ArmenHUB <36188020+ArmenHUB@users.noreply.github.com>
2018-09-03 18:09:38 +0400

    Update device.php

--- device.php
+++ device.php
@@ -5,12 +5,13 @@ require_once "errors.php";
 $all_data = file_get_contents('php://input');
 $income_data = json_decode($all_data);
 
-$answer = $income_data;
-$params = $income_data->params;
+// $answer = $income_data;
+// $params = $income_data->params;
 
 switch ($params->command) {
     case "device_info":
-         $info = device_info();
+         $user_id = $params->user_id;
+         $info = device_info($user_id);
          $answer = ["user_id" => $income_data->user_id, "token" => "0", "error" => "0", "info" => $info];
         break; 
     case "device_list":
@@ -29,21 +30,27 @@ if ($answer['error'] > 0) {
 }
 echo json_encode($answer);
 
-function device_info(){
+function device_info($user_id){
 	$con = new Z_MySQL();
-    $data = $con->queryNoDML("SELECT devices.deviceID AS deviceID, deviceType.text AS devicetype,  devices.deviceName AS deviceName,  devices.location AS location, devices.button_img AS button_img FROM `devices` INNER JOIN `deviceType` ON devices.deviceTypeID = deviceType.deviceTypeID");
-    return $data;	
+    $data = $con->queryNoDML("SELECT deviceID,deviceTypeID FROM `deviceUsers` WHERE `userID` = '$user_id'")[0];
+    if($data){
+    $deviceID = $data['deviceID'];
+    $deviceTypeID = $data['deviceTypeID'];
+    $data1 = $con->queryNoDML("SELECT deviceTypes.text AS DeviceType,deviceParamNames.text As ParamName, deviceParamValues.text As ParamValue FROM `deviceInfo` INNER JOIN  deviceParamNames ON deviceInfo.deviceParamNameID =  deviceParamNames.deviceParamNameID INNER JOIN deviceParamValues ON deviceInfo.deviceParamValueID  = deviceParamValues.deviceParamValueID INNER JOIN deviceTypes ON deviceTypes.deviceTypeID = '$deviceTypeID' WHERE deviceInfo.deviceID = '$deviceID'");
+    return $data1;    	
+    }
 }
 function device_list(){
 	$con = new Z_MySQL();
-    $data = $con->queryNoDML("SELECT devices.deviceID AS deviceID, deviceType.text AS devicetype,  devices.deviceName AS deviceName,  devices.location AS location, devices.deviceStatusID FROM `devices` INNER JOIN `deviceType` ON devices.deviceTypeID = deviceType.deviceTypeID");
+    $data = $con->queryNoDML("SELECT deviceTypes.text AS DeviceType,deviceParamNames.text As ParamName, deviceParamValues.text As ParamValue FROM `deviceInfo` INNER JOIN  deviceParamNames ON deviceInfo.deviceParamNameID =  deviceParamNames.deviceParamNameID INNER JOIN deviceParamValues ON deviceInfo.deviceParamValueID  = deviceParamValues.deviceParamValueID INNER JOIN deviceTypes");
     return $data;	
 }
 function device_register($device_type,$device_name,$location){
    $con = new Z_MySQL();
     if ($con->queryDML("INSERT INTO `deviceType`(`deviceTypeID`, `text`, `langID`) VALUES ('1', '$device_type', '1')")) {
        if ($con->queryDML("INSERT INTO `devices`(`deviceID`, `deviceTypeID`, `deviceName`, `deviceStatusID`, `location`) VALUES ('1', '1', '$device_name', '1','$location')")) {
-          
+          INSERT INTO deviceParamValues ('3','$location')
+          INSERT INTO deviceInfo ('3','1','3')
        }   
     } 
 }
