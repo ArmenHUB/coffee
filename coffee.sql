59b1becf14c0bbce966e4682eff9b7bb23477ae7
ArmenHUB <36188020+ArmenHUB@users.noreply.github.com>
2018-09-06 17:53:28 +0400

    Update coffee.sql

--- coffee.sql
+++ coffee.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Сен 03 2018 г., 17:44
--- Версия сервера: 5.6.38
--- Версия PHP: 5.5.38
+-- Время создания: Сен 06 2018 г., 16:50
+-- Версия сервера: 5.7.20
+-- Версия PHP: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -60,7 +60,8 @@ CREATE TABLE `deviceParamNames` (
 
 INSERT INTO `deviceParamNames` (`deviceParamNameID`, `langID`, `text`) VALUES
 (1, 1, 'location'),
-(2, 1, 'cup');
+(2, 1, 'cup'),
+(3, 1, 'name');
 
 -- --------------------------------------------------------
 
@@ -79,7 +80,8 @@ CREATE TABLE `deviceParamValues` (
 
 INSERT INTO `deviceParamValues` (`deviceParamValueID`, `text`) VALUES
 (1, 'Erevan'),
-(2, '1275');
+(2, '1275'),
+(3, 'iphone');
 
 -- --------------------------------------------------------
 
@@ -123,6 +125,54 @@ INSERT INTO `deviceUsers` (`deviceID`, `userID`, `deviceTypeID`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `errors`
+--
+
+CREATE TABLE `errors` (
+  `errorID` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `langID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `errors`
+--
+
+INSERT INTO `errors` (`errorID`, `text`, `langID`) VALUES
+(0, 'Unknown error', 1),
+(0, 'Неизвестная ошибка', 2),
+(0, 'Անհայտ սխալ', 3),
+(1, 'Invalid characters used', 1),
+(1, 'использованы недопустимые символы', 2),
+(1, 'Օգտագործված անվավեր նիշերը', 3),
+(2, 'Wrong username or password', 1),
+(2, 'Неправильный пользователь или пароль', 2),
+(2, 'Սխալ անուն կամ գաղտնաբառ', 3),
+(3, 'Session expired', 1),
+(3, 'Сессия истекла', 2),
+(3, 'Սեսսիան ավարտված է', 3),
+(4, 'Problem to save data', 1),
+(4, 'Проблема с сохранением информации', 2),
+(4, 'Խնդիր Տվըալների ներդրման հետ', 3),
+(5, 'The user is currently in the system', 1),
+(5, 'Проблема с сохранением информации', 2),
+(5, 'Տվյալ օգտատերը համակարգում է', 3),
+(6, 'Access dinaed', 1),
+(6, 'Доступ закрыт', 2),
+(6, 'Մուտքն արգելված է', 3),
+(7, 'Wrong user', 1),
+(7, 'Wrong user RUS', 2),
+(7, 'Wrong user ARM', 3),
+(8, 'Wrong user type', 1),
+(8, 'Wrong user type RUS', 2),
+(8, 'Wrong user type ARM', 3),
+(9, 'Some fiel is empty', 1),
+(9, 'Some fiel is empty RUS', 2),
+(9, 'Some fiel is empty ARM', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `ingredients`
 --
 
@@ -155,6 +205,39 @@ CREATE TABLE `loggedUsers` (
   `lastAction` timestamp NOT NULL,
   `token` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `menu`
+--
+
+CREATE TABLE `menu` (
+  `menuID` int(11) NOT NULL,
+  `ischildID` int(11) NOT NULL,
+  `userTypeID` int(11) NOT NULL,
+  `langID` int(11) NOT NULL,
+  `text` varchar(50) NOT NULL,
+  `alias` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `menu`
+--
+
+INSERT INTO `menu` (`menuID`, `ischildID`, `userTypeID`, `langID`, `text`, `alias`) VALUES
+(1, 0, 1, 1, 'Owner List', 'owner_list'),
+(1, 1, 1, 1, 'Devices', 'admin_devices'),
+(1, 0, 1, 1, 'VM Types', 'VM_type'),
+(1, 0, 1, 1, 'Logs', 'logs'),
+(2, 0, 2, 1, 'Activate New Device', 'activate_new_device'),
+(2, 1, 2, 1, 'Devices', 'user_devices'),
+(2, 1, 2, 1, 'Incasation', 'incasation'),
+(2, 1, 2, 1, 'Statistics', 'statistics'),
+(2, 0, 2, 1, 'Device Register', 'device_register'),
+(2, 1, 2, 1, 'Collectors', 'collectors'),
+(2, 0, 2, 1, 'Recipe', 'recipe'),
+(2, 0, 2, 1, 'Logs', 'logs');
 
 -- --------------------------------------------------------
 
@@ -240,17 +323,17 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` text NOT NULL,
   `host` varchar(50) NOT NULL,
-  `userTypeID` int(11) NOT NULL
+  `userTypeID` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`userID`, `username`, `password`, `host`, `userTypeID`) VALUES
-(1, 'Armen', 'a123', 'arm.ru', 1),
-(2, 'Arsen', 'a321', 'ars.com', 2),
-(3, 'Arman', 'a456', 'arman.net', 3);
+INSERT INTO `users` (`userID`, `username`, `password`, `host`, `userTypeID`, `email`, `name`) VALUES
+(1, 'Armm555', 'arm876', 'coffeenew', 1, 'arm@mail.ru', 'Armen');
 
 -- --------------------------------------------------------
 
@@ -353,13 +436,24 @@ ALTER TABLE `userParamValues`
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`userID`);
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Индексы таблицы `userTypes`
 --
 ALTER TABLE `userTypes`
   ADD PRIMARY KEY (`userTypeID`,`langID`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
