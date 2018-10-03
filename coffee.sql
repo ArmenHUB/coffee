f55c56b14984f12fc1f81a9c360752df42557048
ArmenHUB <36188020+ArmenHUB@users.noreply.github.com>
2018-10-03 18:12:51 +0400

    Update coffee.sql

--- coffee.sql
+++ coffee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 27, 2018 at 05:24 PM
+-- Generation Time: Oct 03, 2018 at 05:04 PM
 -- Server version: 5.6.38
 -- PHP Version: 5.5.38
 
@@ -32,20 +32,24 @@ CREATE TABLE `action_log` (
   `deviceID` int(11) NOT NULL,
   `ingredientsID` int(11) NOT NULL,
   `count` text NOT NULL,
-  `inc_money` text NOT NULL,
   `measurement_unitsID` int(11) NOT NULL,
-  `timestamp` timestamp NOT NULL
+  `type` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `action_log`
 --
 
-INSERT INTO `action_log` (`deviceID`, `ingredientsID`, `count`, `inc_money`, `measurement_unitsID`, `timestamp`) VALUES
-(1, 1, '500', '100', 1, '2018-10-12 08:10:11'),
-(1, 2, '50', '300', 5, '2018-10-12 10:12:11'),
-(1, 1, '500', '100', 1, '2018-10-13 07:10:11'),
-(1, 11, '3000', '400', 6, '2018-11-22 07:33:11');
+INSERT INTO `action_log` (`deviceID`, `ingredientsID`, `count`, `measurement_unitsID`, `type`, `timestamp`) VALUES
+(1, 1, '500', 5, 'cash', '2018-10-01 12:31:19'),
+(1, 1, '500', 5, 'cash', '2018-10-01 12:31:55'),
+(1, 11, '3000', 6, 'cash', '2018-10-01 12:32:21'),
+(1, 11, '2000', 6, 'cash', '2018-10-01 12:32:42'),
+(1, 15, '-300', 1, 'incasation', '2018-10-01 12:31:19'),
+(1, 15, '-450', 5, 'incasation', '2018-10-01 12:31:55'),
+(3, 4, '700', 1, 'cash', '2018-10-01 12:31:23'),
+(3, 17, '-200', 6, 'incasation', '2018-10-01 12:31:23');
 
 -- --------------------------------------------------------
 
@@ -65,8 +69,7 @@ CREATE TABLE `boardDevice` (
 INSERT INTO `boardDevice` (`deviceID`, `boardID`) VALUES
 (1, 1),
 (2, 3),
-(3, 4),
-(18, 0);
+(3, 4);
 
 -- --------------------------------------------------------
 
@@ -78,7 +81,7 @@ CREATE TABLE `boards` (
   `boardID` int(12) NOT NULL,
   `UID` varchar(25) NOT NULL,
   `serialNumber` varchar(25) NOT NULL,
-  `lastActivity` timestamp NOT NULL
+  `lastActivity` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -86,10 +89,12 @@ CREATE TABLE `boards` (
 --
 
 INSERT INTO `boards` (`boardID`, `UID`, `serialNumber`, `lastActivity`) VALUES
-(1, '991901212499190121111256', '5555555555555555', '2018-09-11 08:44:40'),
-(4, '365687345367576', '2435464546757515', '2018-09-11 08:44:40'),
-(5, '555587345367576', '8547464546757511', '2018-09-11 08:48:40'),
-(6, '151111111555555555555555', '4191510991959198', '2018-09-24 12:15:35');
+(1, '991901212499190121111256', '5555555555555555', '2018-09-11 12:44:40'),
+(4, '365687345367576', '4444444444444444', '2018-09-11 12:44:40'),
+(5, '555587345367576', '8547464546757511', '2018-09-11 12:48:40'),
+(6, '151111111555555555555555', '4191510991959198', '2018-09-24 16:15:35'),
+(16, '564565765675765675675765', '9742453754875395', '0000-00-00 00:00:00'),
+(17, '454545454545454545554545', '5636089479130356', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -101,36 +106,30 @@ CREATE TABLE `deviceInfo` (
   `deviceID` int(11) NOT NULL,
   `deviceParamNameID` int(11) NOT NULL,
   `deviceParamValueID` int(11) NOT NULL,
-  `deviceTypeID` int(11) NOT NULL
+  `vm_type_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `deviceInfo`
 --
 
-INSERT INTO `deviceInfo` (`deviceID`, `deviceParamNameID`, `deviceParamValueID`, `deviceTypeID`) VALUES
-(1, 1, 1, 2),
-(1, 3, 3, 2),
-(1, 4, 4, 2),
-(1, 5, 5, 2),
-(1, 7, 7, 2),
+INSERT INTO `deviceInfo` (`deviceID`, `deviceParamNameID`, `deviceParamValueID`, `vm_type_id`) VALUES
+(1, 1, 1, 14),
+(1, 3, 3, 14),
+(1, 4, 4, 14),
+(1, 5, 5, 14),
+(1, 7, 7, 14),
 (2, 7, 8, 2),
 (3, 7, 9, 14),
 (3, 3, 10, 14),
-(1, 8, 11, 2),
+(1, 8, 11, 14),
 (3, 4, 13, 14),
 (3, 8, 12, 14),
 (3, 5, 14, 14),
-(1, 9, 15, 2),
+(1, 9, 16, 14),
 (3, 1, 19, 14),
 (3, 9, 17, 14),
-(2, 5, 18, 2),
-(18, 3, 20, 2),
-(18, 1, 21, 2),
-(18, 4, 22, 2),
-(18, 7, 23, 2),
-(18, 5, 14, 2),
-(18, 9, 17, 2);
+(2, 5, 18, 2);
 
 -- --------------------------------------------------------
 
@@ -164,7 +163,7 @@ INSERT INTO `deviceParamNames` (`deviceParamNameID`, `langID`, `text`) VALUES
 --
 
 CREATE TABLE `deviceParamValues` (
-  `deviceParamValueID` int(11) NOT NULL,
+  `deviceParamValueID` int(255) NOT NULL,
   `text` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -173,11 +172,11 @@ CREATE TABLE `deviceParamValues` (
 --
 
 INSERT INTO `deviceParamValues` (`deviceParamValueID`, `text`) VALUES
-(1, '12.120.242'),
-(3, 'Apple A12'),
-(4, 'Sebastia Street'),
+(1, '40.179575052100255-44.516851878356874'),
+(3, 'Apple A121000'),
+(4, 'Sebastia Street1000'),
 (5, '1'),
-(7, '2005-07-13 08:00:99'),
+(7, '2020-07-13 08:00:99'),
 (8, '2021-05-17 09:00:00'),
 (9, '2009-07-19 09:33:00'),
 (10, 'Samsung'),
@@ -188,12 +187,8 @@ INSERT INTO `deviceParamValues` (`deviceParamValueID`, `text`) VALUES
 (15, 'images/loacation_icon_active.png'),
 (16, 'images/loacation_icon_warning.png'),
 (17, 'images/loacation_icon_error.png'),
-(18, '2'),
-(19, '12.16.12.16'),
-(20, ' Motorolla'),
-(21, ' 12.15.12.15'),
-(22, ' Abovyan 75'),
-(23, ' 2018/12/12');
+(18, '1'),
+(19, '12.16.12.16');
 
 -- --------------------------------------------------------
 
@@ -225,18 +220,17 @@ INSERT INTO `deviceTypes` (`deviceTypeID`, `langID`, `text`, `image`) VALUES
 CREATE TABLE `deviceUsers` (
   `deviceID` int(11) NOT NULL,
   `userID` int(11) NOT NULL,
-  `deviceTypeID` int(11) NOT NULL
+  `vm_type_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `deviceUsers`
 --
 
-INSERT INTO `deviceUsers` (`deviceID`, `userID`, `deviceTypeID`) VALUES
-(1, 1, 2),
+INSERT INTO `deviceUsers` (`deviceID`, `userID`, `vm_type_id`) VALUES
+(1, 1, 14),
 (2, 3, 2),
-(3, 1, 2),
-(18, 1, 2);
+(3, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -287,7 +281,7 @@ INSERT INTO `errors` (`errorID`, `text`, `langID`) VALUES
 (9, 'Some fiel is empty ARM', 3),
 (10, 'Unknown device ARM', 1),
 (10, 'Unknown device Rus', 2),
-(10, 'Unknown device ARM', 3),
+(10, 'Unknown device ENG', 3),
 (11, 'Unknown vm type', 1),
 (11, 'Unknown  vm type Rus', 2),
 (11, 'Unknown  vm type ARM', 3);
@@ -310,23 +304,18 @@ CREATE TABLE `ingredients` (
 --
 
 INSERT INTO `ingredients` (`ingredientsID`, `ingredientNameID`, `unitVending`, `unitCollector`) VALUES
-(1, 2, '12', '11'),
-(2, 3, '8', '9'),
-(3, 2, '5', '10'),
-(4, 3, '55', '55'),
-(11, 5, '22', '33'),
-(12, 3, '0', '0'),
-(13, 6, '0', '0'),
-(14, 7, '0', '0'),
-(15, 3, '0', '0'),
-(16, 6, '0', '0'),
-(17, 7, '0', '0'),
+(1, 3, '0', '0'),
+(3, 2, '0', '0'),
+(4, 3, '0', '0'),
+(8, 1, '0', '0'),
+(9, 3, '0', '0'),
+(10, 2, '0', '0'),
+(11, 5, '0', '0'),
+(15, 6, '0', '0'),
+(16, 5, '0', '0'),
+(17, 6, '0', '0'),
 (18, 3, '0', '0'),
-(19, 6, '0', '0'),
-(20, 7, '0', '0'),
-(21, 3, '0', '0'),
-(22, 5, '0', '0'),
-(23, 6, '0', '0');
+(20, 6, '0', '0');
 
 -- --------------------------------------------------------
 
@@ -349,9 +338,9 @@ INSERT INTO `ingredientsName` (`ingredientsNameID`, `text`, `langID`) VALUES
 (2, 'Coffee', 1),
 (3, 'Cup', 1),
 (4, 'Milk', 1),
-(5, 'Money', 1),
-(6, 'Money_cash_in', 1),
-(7, 'spent_money', 1);
+(5, 'Money_cash_in', 1),
+(6, 'inc_money', 1),
+(7, 'Vending_money', 1);
 
 -- --------------------------------------------------------
 
@@ -381,7 +370,7 @@ CREATE TABLE `loggedUsers` (
 --
 
 INSERT INTO `loggedUsers` (`userID`, `lastAction`, `token`) VALUES
-(1, '2018-09-27 14:20:09', 'bScEt0VrWE');
+(1, '2018-10-03 13:59:28', 'YHl5LjoLSM');
 
 -- --------------------------------------------------------
 
@@ -405,7 +394,7 @@ INSERT INTO `measurement_units` (`measurement_unitsID`, `text`, `langID`) VALUES
 (3, 'package', 1),
 (4, 'liter', 1),
 (5, 'thing', 1),
-(6, 'dram', 1);
+(6, 'money', 1);
 
 -- --------------------------------------------------------
 
@@ -431,13 +420,11 @@ INSERT INTO `menu` (`menuID`, `ischildID`, `userTypeID`, `langID`, `text`, `alia
 (2, 1, 1, 1, 'Devices', 'admin_devices'),
 (3, 0, 1, 1, 'VM Types', 'vm_type'),
 (4, 0, 1, 1, 'Logs', 'logs_admin'),
-(5, 0, 2, 1, 'Activate New Device', 'activate_new_device'),
-(6, 1, 2, 1, 'Devices', 'user_devices'),
+(5, 0, 2, 1, 'Device Register', 'activate_new_device'),
+(6, 0, 2, 1, 'Devices', 'user_devices'),
 (7, 1, 2, 1, 'Incasation', 'incasation'),
-(8, 1, 2, 1, 'Statistics', 'statistics'),
-(9, 0, 2, 1, 'Device Register', 'device_register'),
+(8, 0, 2, 1, 'Statistics', 'statistics'),
 (10, 0, 2, 1, 'Collectors', 'collectors'),
-(11, 0, 2, 1, 'Recipe', 'recipe'),
 (12, 0, 2, 1, 'Logs', 'logs_owner');
 
 -- --------------------------------------------------------
@@ -499,6 +486,15 @@ CREATE TABLE `userInfo` (
   `userParamValueID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `userInfo`
+--
+
+INSERT INTO `userInfo` (`userID`, `userParamNameID`, `userParamValueID`) VALUES
+(1, 1, 1),
+(3, 1, 2),
+(4, 1, 3);
+
 -- --------------------------------------------------------
 
 --
@@ -511,6 +507,13 @@ CREATE TABLE `userParamNames` (
   `text` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `userParamNames`
+--
+
+INSERT INTO `userParamNames` (`userParamNameID`, `langID`, `text`) VALUES
+(1, 1, 'surname');
+
 -- --------------------------------------------------------
 
 --
@@ -521,6 +524,15 @@ CREATE TABLE `userParamValues` (
   `userParamValueID` int(11) NOT NULL,
   `text` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `userParamValues`
+--
+
+INSERT INTO `userParamValues` (`userParamValueID`, `text`) VALUES
+(1, 'Manukyan'),
+(2, 'Gevorgyan'),
+(3, 'Vardanyan');
 
 -- --------------------------------------------------------
 
@@ -546,7 +558,8 @@ INSERT INTO `users` (`userID`, `username`, `password`, `host`, `userTypeID`, `em
 (1, 'Owner', '098f6bcd4621d373cade4e832627b4f6', 'coffeev2', 2, 'arm@mail.rukkk', 'Armen'),
 (3, 'Admin', '098f6bcd4621d373cade4e832627b4f6', 'coffeev2', 1, 'ars@mail.ru', 'Gago'),
 (4, 'Collector', 'd41d8cd98f00b204e9800998ecf8427e', 'coffeev2', 3, 'ars@mail.ru1111', 'Gago5411111222'),
-(14, 'Arsen12', 'd41d8cd98f00b204e9800998ecf8427es', 'coffeev2', 2, 'ars@23@nm.ru', 'Arsen');
+(14, 'Arsen12', 'd41d8cd98f00b204e9800998ecf8427es', 'coffeev2', 2, 'ars@23@nm.ru', 'Arsen'),
+(15, 'Abo12', '098f6bcd4621d373cade4e832627b4f6', 'coffeev2', 2, 'arm@z-sof.net', 'Abo');
 
 -- --------------------------------------------------------
 
@@ -591,7 +604,8 @@ INSERT INTO `vm_types` (`vm_type_id`, `name`, `button_count`, `image`) VALUES
 (2, 'VM85555555', 14, 'vm85.png'),
 (3, '10', 15, 'vm.jpg'),
 (14, 'test 1222', 0, '0'),
-(17, 'f', 0, '0');
+(17, 'f', 0, '0'),
+(18, 'ssss', 0, '0');
 
 -- --------------------------------------------------------
 
@@ -624,7 +638,10 @@ INSERT INTO `vm_type_ingredients` (`vm_type_id`, `ingredientsID`) VALUES
 (16, 20),
 (17, 21),
 (17, 22),
-(17, 23);
+(17, 23),
+(18, 24),
+(18, 25),
+(18, 26);
 
 --
 -- Indexes for dumped tables
@@ -766,7 +783,7 @@ ALTER TABLE `vm_types`
 -- AUTO_INCREMENT for table `boards`
 --
 ALTER TABLE `boards`
-  MODIFY `boardID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `boardID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `deviceParamNames`
@@ -778,7 +795,7 @@ ALTER TABLE `deviceParamNames`
 -- AUTO_INCREMENT for table `deviceParamValues`
 --
 ALTER TABLE `deviceParamValues`
-  MODIFY `deviceParamValueID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `deviceParamValueID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 
 --
 -- AUTO_INCREMENT for table `deviceTypes`
@@ -790,13 +807,13 @@ ALTER TABLE `deviceTypes`
 -- AUTO_INCREMENT for table `deviceUsers`
 --
 ALTER TABLE `deviceUsers`
-  MODIFY `deviceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `deviceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `ingredients`
 --
 ALTER TABLE `ingredients`
-  MODIFY `ingredientsID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `ingredientsID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `ingredientsName`
@@ -823,16 +840,22 @@ ALTER TABLE `recipeNames`
   MODIFY `recipeNameID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `userParamValues`
+--
+ALTER TABLE `userParamValues`
+  MODIFY `userParamValueID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `vm_types`
 --
 ALTER TABLE `vm_types`
-  MODIFY `vm_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `vm_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
