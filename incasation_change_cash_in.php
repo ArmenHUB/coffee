f2228b9000c44e1bcfdb8e895abb14bfb3c09252
ArmenHUB <36188020+ArmenHUB@users.noreply.github.com>
2018-12-28 15:09:53 +0400

    Update incasation_change_cash_in.php

--- incasation_change_cash_in.php
+++ incasation_change_cash_in.php
@@ -1,3 +1,4 @@
 SELECT SUM(case when `type` IN ('cash','vending') AND `action_log`.`ingredientsID` = '3' THEN `count` END) cup,
-       SUM(case when `type` IN ('cash','incasation') AND `action_log`.`ingredientsID` IN ('5','6') THEN `count` END) cash_in,
-       SUM(case when `type` = 'vending' AND `action_log`.`ingredientsID` = '7' THEN `count` END) vending_money FROM `action_log`
+SUM(case when `type` IN ('cash','incasation') AND `action_log`.`ingredientsID` IN ('5','6') THEN `count` END) cash_in,
+SUM(case when `type` = 'vending' AND `action_log`.`ingredientsID` = '7' THEN `count` END) vending_money FROM `action_log`
+WHERE `deviceID` = '5'
